f4424191a8fe4d8d1205117bb0e845ae5c6ebf30
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-04 16:26:21 -0300

    refactor: add `.` in exception message from `InvalidCustomerDataException`

--- src/Exceptions/InvalidCustomerDataException.php
+++ src/Exceptions/InvalidCustomerDataException.php
@@ -8,12 +8,12 @@ final class InvalidCustomerDataException extends AsaasException
 {
     public static function missingField(string $field): self
     {
-        return new self("Required field '{$field}' is missing", 400);
+        return new self("Required field '{$field}' is missing.", 400);
     }
 
     public static function invalidFormat(string $field, ?string $message = null): self
     {
-        $defaultMessage = "Field '{$field}' has invalid format";
+        $defaultMessage = "Field '{$field}' has invalid format.";
 
         return new self($message ?? $defaultMessage, 400);
     }
