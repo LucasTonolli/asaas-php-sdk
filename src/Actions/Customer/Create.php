faf14b9be8d53b16ac05aeb1bfe2480b24977b82
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-07 10:26:45 -0300

    fix: change visibility in Actions construct attributes

--- src/Actions/Customer/Create.php
+++ src/Actions/Customer/Create.php
@@ -6,16 +6,13 @@ namespace AsaasPhpSdk\Actions\Customer;
 
 use AsaasPhpSdk\Actions\AbstractAction;
 use AsaasPhpSdk\DTOs\Customer\CreateCustomerDTO;
-use AsaasPhpSdk\Exceptions\ApiException;
 use AsaasPhpSdk\Helpers\ResponseHandler;
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ConnectException;
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Exception\RequestException;
+
 
 final class Create extends AbstractAction
 {
-    public function __construct(private readonly Client $client, private readonly ResponseHandler $responseHandler) {}
+    public function __construct(protected readonly Client $client, protected readonly ResponseHandler $responseHandler) {}
 
     /**
      * Create a new customer in Asaas
@@ -23,39 +20,11 @@ final class Create extends AbstractAction
      * @param  CreateCustomerDTO  $data  Customer data
      * @return array Customer data from API
      *
-     * @throws AsaasException,
-     * @throws GuzzleException
      */
     public function handle(CreateCustomerDTO $data): array
     {
-        try {
-            $response = $this->client->post('customers', [
-                'json' => $data->toArray(),
-            ]);
-
-            return $this->responseHandler->handle($response);
-        } catch (RequestException $e) {
-            if ($e->hasResponse()) {
-                return $this->responseHandler->handle($e->getResponse());
-            }
-
-            throw new ApiException(
-                'Request failed: ' . $e->getMessage(),
-                $e->getCode(),
-                $e
-            );
-        } catch (ConnectException $e) {
-            throw new ApiException(
-                'Failed to connect to Asaas API: ' . $e->getMessage(),
-                0,
-                $e
-            );
-        } catch (GuzzleException $e) {
-            throw new ApiException(
-                'HTTP client error: ' . $e->getMessage(),
-                $e->getCode(),
-                $e
-            );
-        }
+        return $this->executeRequest(
+            fn() => $this->client->post('customers', ['json' => $data->toArray()])
+        );
     }
 }
--- src/Actions/Customer/ListCustomersAction.php
+++ src/Actions/Customer/ListCustomersAction.php
@@ -4,46 +4,17 @@ namespace AsaasPhpSdk\Actions\Customer;
 
 use AsaasPhpSdk\Actions\AbstractAction;
 use AsaasPhpSdk\DTOs\Customer\ListCustomersDTO;
-use AsaasPhpSdk\Exceptions\ApiException;
 use AsaasPhpSdk\Helpers\ResponseHandler;
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ConnectException;
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Exception\RequestException;
+
 
 final class ListCustomersAction extends AbstractAction
 {
-	public function __construct(private readonly Client $client, private readonly ResponseHandler $responseHandler) {}
+	public function __construct(protected readonly Client $client, protected readonly ResponseHandler $responseHandler) {}
 	public function handle(ListCustomersDTO $data): array
 	{
-		try {
-			$response = $this->client->get('customers', [
-				'query' => $data->toArray(),
-			]);
-
-			return $this->responseHandler->handle($response);
-		} catch (RequestException $e) {
-			if ($e->hasResponse()) {
-				return $this->responseHandler->handle($e->getResponse());
-			}
-
-			throw new ApiException(
-				'Request failed: ' . $e->getMessage(),
-				$e->getCode(),
-				$e
-			);
-		} catch (ConnectException $e) {
-			throw new ApiException(
-				'Failed to connect to Asaas API: ' . $e->getMessage(),
-				0,
-				$e
-			);
-		} catch (GuzzleException $e) {
-			throw new ApiException(
-				'HTTP client error: ' . $e->getMessage(),
-				$e->getCode(),
-				$e
-			);
-		}
+		return $this->executeRequest(
+			fn() => $this->client->get('customers', ['query' => $data->toArray()])
+		);
 	}
 }
