1160b197cf1412a181108abff89adf15bf0454f6
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-09 13:32:53 -0300

    test: add Integration Update Customer

---
+++ tests/Integration/Http/Customers/UpdateCustomeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+describe('Update Customer', function () {
+	beforeEach(function () {
+		$config = sandboxConfig();
+		$this->asaasClient = new AsaasPhpSdk\AsaasClient($config);
+	});
+
+	it('updates a customer successfully', function () {
+		$createCustomerResponse = $this->asaasClient->customer()->create([
+			'name' => 'John Doe' . uniqid(),
+			'email' => 'john_doe' . uniqid() . '@example.com',
+			'cpfCnpj' => '898.879.660-88',
+		]);
+
+		$response = $this->asaasClient->customer()->update($createCustomerResponse['id'], [
+			'name' => 'John Doe Updated',
+			'email' => 'john_doe_updated@example.com',
+		]);
+		expect($response['name'])->toBe('John Doe Updated')
+			->and($response['email'])->toBe('john_doe_updated@example.com');
+	});
+
+	it('throws an exception when the customer is not found (404)', function () {
+		$this->asaasClient->customer()->update('cus_notfound', [
+			'name' => 'John Doe Updated',
+		]);
+	})->throws(\AsaasPhpSdk\Exceptions\NotFoundException::class, 'Resource not found');
+
+	it('throws an exception when the customer ID is empty', function () {
+		$this->asaasClient->customer()->update('', [
+			'name' => 'John Doe Updated',
+		]);
+	})->throws(\InvalidArgumentException::class, 'Customer ID cannot be empty');
+});
