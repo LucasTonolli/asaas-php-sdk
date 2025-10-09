4ff9092b1af2cdc433311dd902800866e0aa3a1a
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-09 09:58:34 -0300

    test: add Integration Get Customer Test

---
+++ tests/Integration/Http/Customers/GetCustomerTest.php
@@ -0,0 +1,101 @@
+<?php
+
+describe('Get Customer', function () {
+	beforeEach(function () {
+		$config = sandboxConfig();
+		$this->asaasClient = new AsaasPhpSdk\AsaasClient($config);
+	});
+
+	it('retrieves a customer successfully (200)', function () {
+		$customerId = null;
+
+		$getCustomersResponse = $this->asaasClient->customer()->list([
+			'limit' => 1,
+			'cpfCnpj' => '00264272000107',
+		]);
+
+
+		if (empty($getCustomersResponse['data'])) {
+			$createCustomerResponse = $this->asaasClient->customer()->create([
+				'name' => 'Maria Oliveira',
+				'cpfCnpj' => '00264272000107',
+			]);
+			$customerId = $createCustomerResponse['id'];
+		} else {
+			$customerId = $getCustomersResponse['data'][0]['id'];
+		}
+
+		$response = $this->asaasClient->customer()->get($customerId);
+		expect($response)->toBeArray()
+			->and($response['id'])->toBe($customerId)
+			->and($response['name'])->toBe('Maria Oliveira')
+			->and($response['cpfCnpj'])->toBe('00264272000107')
+			->and($response)->toHaveKeys([
+				'object',
+				'dateCreated',
+				'email',
+				'phone',
+				'mobilePhone',
+				'address',
+				'addressNumber',
+				'complement',
+				'province',
+				'city',
+				'cityName',
+				'state',
+				'country',
+				'postalCode',
+				'personType',
+				'deleted',
+				'additionalEmails',
+				'externalReference',
+				'notificationDisabled',
+				'observations',
+			]);
+	});
+
+	it('throws an exception when the customer is not found (404)', function () {
+		expect(fn() => $this->asaasClient->customer()->get('invalid-customer-id'))->toThrow(AsaasPhpSdk\Exceptions\NotFoundException::class, 'Resource not found');
+	});
+
+	it('throws an exception when the customer ID is empty', function () {
+		expect(fn() => $this->asaasClient->customer()->get(''))->toThrow(\InvalidArgumentException::class, 'Customer ID cannot be empty');
+	});
+
+	it('matches the expected response structure', function () {
+		$getCustomersResponse = $this->asaasClient->customer()->list([
+			'limit' => 1,
+			'cpfCnpj' => '00264272000107',
+		]);
+
+		$customerId = $getCustomersResponse['data'][0]['id'];
+
+		$response = $this->asaasClient->customer()->get($customerId);
+		expect($response['id'])->toBe($customerId);
+		expect($response)->toHaveKeys([
+			'object',
+			'id',
+			'dateCreated',
+			'name',
+			'email',
+			'phone',
+			'mobilePhone',
+			'address',
+			'addressNumber',
+			'complement',
+			'province',
+			'city',
+			'cityName',
+			'state',
+			'country',
+			'postalCode',
+			'cpfCnpj',
+			'personType',
+			'deleted',
+			'additionalEmails',
+			'externalReference',
+			'notificationDisabled',
+			'observations',
+		]);
+	});
+});
