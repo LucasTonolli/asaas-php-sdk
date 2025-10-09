5169d15ae74790dacfd9722a81d1a711dc96af85
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-09 20:37:30 -0300

    docs: phpdocs UpdateCustomerAction

--- src/Actions/Customers/UpdateCustomerAction.php
+++ src/Actions/Customers/UpdateCustomerAction.php
@@ -9,6 +9,24 @@ use AsaasPhpSdk\DTOs\Customers\UpdateCustomerDTO;
 
 final class UpdateCustomerAction extends AbstractAction
 {
+    /**
+     * Updates an existing customer by their ID.
+     *
+     * This action performs a pre-request validation on the ID and sends a PUT
+     * request to the 'customers/{id}' endpoint with the new data.
+     *
+     * @see https://docs.asaas.com/reference/atualizar-cliente-existente Official Asaas API Documentation
+     *
+     * @param  string  $id The unique identifier of the customer to be updated.
+     * @param  UpdateCustomerDTO  $data A DTO containing the customer data to be updated.
+     * @return array An array containing the full, updated data of the customer.
+     *
+     * @throws \InvalidArgumentException if the provided customer ID is empty.
+     * @throws \AsaasPhpSdk\Exceptions\ApiException
+     * @throws \AsaasPhpSdk\Exceptions\AuthenticationException
+     * @throws \AsaasPhpSdk\Exceptions\NotFoundException if the customer with the given ID does not exist.
+     * @throws \AsaasPhpSdk\Exceptions\ValidationException if the data provided is invalid.
+     */
     public function handle(string $id, UpdateCustomerDTO $data): array
     {
         if (empty(trim($id))) {
@@ -16,7 +34,7 @@ final class UpdateCustomerAction extends AbstractAction
         }
 
         return $this->executeRequest(
-            fn () => $this->client->put('customers/'.rawurlencode($id), ['json' => $data->toArray()])
+            fn() => $this->client->put('customers/' . rawurlencode($id), ['json' => $data->toArray()])
         );
     }
 }
