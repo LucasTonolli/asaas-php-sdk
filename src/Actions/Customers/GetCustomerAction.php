546fb7a8492f527e04b524408ca8c9f47ebf0f4c
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-09 20:34:40 -0300

    docs: phpdocs GetCustomerAction

--- src/Actions/Customers/GetCustomerAction.php
+++ src/Actions/Customers/GetCustomerAction.php
@@ -8,6 +8,22 @@ use AsaasPhpSdk\Actions\AbstractAction;
 
 final class GetCustomerAction extends AbstractAction
 {
+    /**
+     * Retrieves a single customer by their ID.
+     *
+     * This action performs a pre-request validation to ensure the ID is not
+     * empty and then sends a GET request to the 'customers/{id}' endpoint.
+     *
+     * @see https://docs.asaas.com/reference/recuperar-um-unico-cliente Official Asaas API Documentation
+     *
+     * @param  string  $id The unique identifier of the customer to be retrieved.
+     * @return array An array containing the data of the specified customer.
+     *
+     * @throws \InvalidArgumentException if the provided customer ID is empty.
+     * @throws \AsaasPhpSdk\Exceptions\ApiException
+     * @throws \AsaasPhpSdk\Exceptions\AuthenticationException
+     * @throws \AsaasPhpSdk\Exceptions\NotFoundException if the customer with the given ID does not exist.
+     */
     public function handle(string $id): array
     {
         if (empty(trim($id))) {
@@ -15,7 +31,7 @@ final class GetCustomerAction extends AbstractAction
         }
 
         return $this->executeRequest(
-            fn () => $this->client->get('customers/'.rawurlencode($id))
+            fn() => $this->client->get('customers/' . rawurlencode($id))
         );
     }
 }
