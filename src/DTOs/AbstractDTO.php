996ba1f5ded7ce504eec3e29391248ab215c6c8e
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-09 18:14:56 -0300

    feat: add helpers in `AbstractDTO`

--- src/DTOs/AbstractDTO.php
+++ src/DTOs/AbstractDTO.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace AsaasPhpSdk\DTOs;
 
 use AsaasPhpSdk\DTOs\Attributes\ToArrayMethodAttribute;
+use AsaasPhpSdk\Helpers\DataSanitizer;
 
 abstract class AbstractDTO implements DTOContract
 {
@@ -46,11 +47,42 @@ abstract class AbstractDTO implements DTOContract
             $data[$key] = $valueObjectClass::from($data[$key]);
         } catch (\Exception $e) {
             throw new \InvalidArgumentException(
-                "Invalid format for '{$key}': ".$e->getMessage(),
+                "Invalid format for '{$key}': " . $e->getMessage(),
                 0,
                 $e
             );
         }
+    }
+
+    protected static function optionalString(array $data, string $key): ?string
+    {
+        return array_key_exists($key, $data)
+            ? DataSanitizer::sanitizeString($data[$key])
+            : null;
+    }
+
+    protected static function optionalBoolean(array $data, string $key): ?bool
+    {
+        return array_key_exists($key, $data)
+            ? DataSanitizer::sanitizeBoolean($data[$key])
+            : null;
+    }
+
+    protected static function optionalOnlyDigits(array $data, string $key): ?string
+    {
+        if (!array_key_exists($key, $data) || $data[$key] === null || $data[$key] === '') {
+            return null;
+        }
+
+        return DataSanitizer::onlyDigits($data[$key]);
+    }
+
+    protected static function optionalInteger(array $data, string $key): ?int
+    {
+        if (!array_key_exists($key, $data) || $data[$key] === null || $data[$key] === '') {
+            return null;
+        }
+        return DataSanitizer::sanitizeInteger($data[$key]);
     }
 
     abstract protected static function sanitize(array $data): array;
