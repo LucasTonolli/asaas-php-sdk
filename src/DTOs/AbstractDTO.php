0c5525196f007a29c248bd915886cfba3dcae46f
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-09 12:53:34 -0300

    fix: remane property in `AbstractDTO`

--- src/DTOs/AbstractDTO.php
+++ src/DTOs/AbstractDTO.php
@@ -8,50 +8,50 @@ use AsaasPhpSdk\DTOs\Attributes\ToArrayMethodAttribute;
 
 abstract class AbstractDTO implements DTOContract
 {
-    public function toArray(): array
-    {
-        $reflection = new \ReflectionClass($this);
-        $properties = $reflection->getProperties(\ReflectionProperty::IS_PUBLIC);
-        $result = [];
+	public function toArray(): array
+	{
+		$reflection = new \ReflectionClass($this);
+		$properties = $reflection->getProperties(\ReflectionProperty::IS_PUBLIC);
+		$result = [];
 
-        foreach ($properties as $property) {
-            $key = $property->getName();
-            $value = $property->getValue($this);
+		foreach ($properties as $property) {
+			$key = $property->getName();
+			$value = $property->getValue($this);
 
-            if ($value === null) {
-                continue;
-            }
+			if ($value === null) {
+				continue;
+			}
 
-            $attributes = $property->getAttributes(ToArrayMethodAttribute::class);
-            if (! empty($attributes)) {
-                $method = $attributes[0]->newInstance()->methodName;
-                $result[$key] = $value->{$method}();
-            } elseif (is_object($value) && method_exists($value, 'value')) {
-                $result[$key] = $value->value();
-            } else {
-                $result[$key] = $value;
-            }
-        }
+			$attributes = $property->getAttributes(ToArrayMethodAttribute::class);
+			if (! empty($attributes)) {
+				$method = $attributes[0]->newInstance()->method;
+				$result[$key] = $value->{$method}();
+			} elseif (is_object($value) && method_exists($value, 'value')) {
+				$result[$key] = $value->value();
+			} else {
+				$result[$key] = $value;
+			}
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 
-    protected static function validateValueObject(array &$data, string $key, string $valueObjectClass): void
-    {
-        if (! isset($data[$key]) || $data[$key] === null) {
-            return;
-        }
+	protected static function validateValueObject(array &$data, string $key, string $valueObjectClass): void
+	{
+		if (! isset($data[$key]) || $data[$key] === null) {
+			return;
+		}
 
-        try {
-            $data[$key] = $valueObjectClass::from($data[$key]);
-        } catch (\Exception $e) {
-            throw new \InvalidArgumentException(
-                "Invalid format for '{$key}': ".$e->getMessage(),
-                0,
-                $e
-            );
-        }
-    }
+		try {
+			$data[$key] = $valueObjectClass::from($data[$key]);
+		} catch (\Exception $e) {
+			throw new \InvalidArgumentException(
+				"Invalid format for '{$key}': " . $e->getMessage(),
+				0,
+				$e
+			);
+		}
+	}
 
-    abstract protected static function sanitize(array $data): array;
+	abstract protected static function sanitize(array $data): array;
 }
