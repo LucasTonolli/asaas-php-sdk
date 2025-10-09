34c6cfcdccc8035a6efdd06f31a88e30dbaf479c
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-09 20:59:44 -0300

    docs: phpdocs InvalidCnpjException

--- src/Exceptions/InvalidCnpjException.php
+++ src/Exceptions/InvalidCnpjException.php
@@ -3,6 +3,11 @@
 namespace AsaasPhpSdk\Exceptions;
 
 /**
- * Thrown when CNPJ validation fails
+ * Represents an error for an invalid CNPJ value.
+ *
+ * This exception is typically thrown by the `Cnpj` Value Object when a given
+ * string does not conform to a valid CNPJ format (e.g., incorrect number of
+ * digits or an invalid checksum).
  */
+
 class InvalidCnpjException extends InvalidValueObjectException {}
