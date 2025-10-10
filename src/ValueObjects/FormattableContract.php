d5fb00946e48a3f2332166324e231088486fe983
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-09 21:21:41 -0300

    docs: phpdocs FormattableContract VO

--- src/ValueObjects/FormattableContract.php
+++ src/ValueObjects/FormattableContract.php
@@ -2,6 +2,17 @@
 
 namespace AsaasPhpSdk\ValueObjects;
 
+/**
+ * Defines a contract for Value Objects that have a user-friendly formatted representation.
+ *
+ * This interface should be implemented by Value Objects that store a raw, sanitized
+ * value but can also provide a "pretty-printed" version for display purposes.
+ *
+ * @example
+ * $cpf = Cpf::from('12345678900');
+ * echo $cpf->value();     // Outputs: '12345678900'
+ * echo $cpf->formatted(); // Outputs: '123.456.789-00'
+ */
 interface FormattableContract
 {
     public function formatted(): string;
