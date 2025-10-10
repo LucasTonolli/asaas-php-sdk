92cb898e9d3fcebd94722d5d837d0110ce14acca
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-09 21:00:12 -0300

    docs: phpdocs InvalidCpfException

--- src/Exceptions/InvalidCpfException.php
+++ src/Exceptions/InvalidCpfException.php
@@ -3,6 +3,11 @@
 namespace AsaasPhpSdk\Exceptions;
 
 /**
- * Thrown when CPF validation fails
+ * Represents an error for an invalid CPF value.
+ *
+ * This exception is typically thrown by the `Cpf` Value Object when a given
+ * string does not conform to a valid CPF format (e.g., incorrect number of
+ * digits or an invalid checksum).
  */
+
 class InvalidCpfException extends InvalidValueObjectException {}
